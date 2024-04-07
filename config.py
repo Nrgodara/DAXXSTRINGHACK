8d114f88bfa7366120804dd9875147e0381ad893
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-04-07 13:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     API_HASH = "019e63043383f3b5e7c89c2431ec23dc"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    #MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URL = "mongodb+srv://mrmahendrji:Mahendra321@cluster0.miaccbn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["1280494242"])
